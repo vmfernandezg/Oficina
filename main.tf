9096a8b3c3728e1a4639e58e3181eae337139b00
vfernandezg@gmail.com <vfernandezg@gmail.com>
2020-07-05 20:30:33 +0200

    sixth commit

--- main.tf
+++ main.tf
@@ -1,3 +1,12 @@
+terraform {
+  backend "azurerm" {
+    resource_group_name   = "pruebarg"
+    storage_account_name  = "vmfernandezg"
+    container_name        = "terraform"
+    key                   = "terraform.tfstate"
+  }
+}
+
 provider "azurerm" {
   version = "~>2.0" 
   subscription_id = "${var.sub}"
